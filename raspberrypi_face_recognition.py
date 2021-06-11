acb9cb7746c4be0f019483e37902981074753cf7
Naomi Hernandez <47608592+nhern7@users.noreply.github.com>
2021-06-11 17:06:49 -0500

    Update raspberrypi_face_recognition.py

--- raspberrypi_face_recognition.py
+++ raspberrypi_face_recognition.py
@@ -1,14 +1,3 @@
-"""
-This program will print out the names of anyone it recognizes to the console
-
-REQUIRED: 
-    raspberry pi 2 (or greater),
-    picamera[array] module,
-    face_recognition,
-
-credit: https://github.com/ageitgey/face_recognition
-"""
-
 import face_recognition
 import picamera
 import numpy as np
